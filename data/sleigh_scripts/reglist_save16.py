a2f90e9da09436fcfa5e789a563895f0f83a3dfc
James Chambers <jameschambers2@gmail.com>
2024-01-25 16:55:56 -0500

    more general entry points for SAVE[16] reglist

--- data/sleigh_scripts/reglist_save16.py
+++ data/sleigh_scripts/reglist_save16.py
@@ -14,32 +14,28 @@ REGS = """zero
 
 
 def main():
+    base_name = 'reglist_sv16'
     sv16_regs = ['fp', 'ra'] + REGS[16:30]
-    #print(sv16_regs)
-    #print(len(sv16_regs))
 
-    for i in range(15, -1, -1):
-        name = f'reglist_sv16_{i:02}'
-        next_name = f'reglist_sv16_{i+1:02}'
+    for i in range(len(sv16_regs) - 1, -1, -1):
+        name = f'{base_name}_{i:02}'
+        next_name = f'{base_name}_{i+1:02}'
         reg = f'{sv16_regs[i]}'
 
-        if i == 0:
-            sp_write = f'*:$(REGSIZE) (sp - $(REGSIZE)) = {reg};'
-        else:
-            sp_write = f'*:$(REGSIZE) (sp - $(REGSIZE)*({i+1}-rt1_raw)) = {reg};'
-
-        if i == 0:
-            print(f'{name}: \t\t\tis count = 0 {{}}')
-            # skip for rt=1
-            print(f'{name}: ,{next_name}\tis rt1_raw = 1 & {next_name} {{}}')
-            print(f'{name}: ,fp\t\t\tis rt1_raw = 0 & count = 1 & fp\t\t{{{sp_write}}}')
-            print(f'{name}: ,fp,{next_name}\tis rt1_raw = 0 & fp & {next_name}\t{{{sp_write}}}')
-            continue
+        sp_write = f'*:$(REGSIZE) (sp - $(REGSIZE)*({i+1}-rt1_raw)) = {reg};'
 
         print(f'{name}: {reg}\t\t\tis count = {i+1} - rt1_raw & rt1_raw & {reg}\t{{{sp_write}}}')
 
-        if i != 15:
+        if i != len(sv16_regs) - 1:
             print(f'{name}: {reg},{next_name}\tis rt1_raw & {reg} & {next_name}\t{{{sp_write}}}')
+
+    # empty register list case for count = 0
+    print(f'{base_name}: \t\t\tis count = 0 {{}}')
+
+    # entry points to first register (fp or ra)
+    for i in range(1, -1, -1):
+        next_name = f'{base_name}_{i:02}'
+        print(f'{base_name}: ,{next_name}\tis rt1_raw = {i} & {next_name} {{}}')
 
 
 if __name__ == '__main__':
